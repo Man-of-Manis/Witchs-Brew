ddf542cd849d4556238ecd1e697f87494ecd6082
Man-of-Manis <Man-of-Manis@users.noreply.github.com>
2020-05-14 14:19:56 -0700

    Main Menu Prototype and Level Editor Fixes

--- Assets/Scripts/UI/LoadingScreenBar.cs
+++ Assets/Scripts/UI/LoadingScreenBar.cs
@@ -12,7 +12,7 @@ public class LoadingScreenBar : MonoBehaviour
     [SerializeField] private float levelLoadDelay = 0.25f;
     [SerializeField] private float LoadingIconFadeTime = 0.2f;
 
-    public static LoadingScreenBar loadingScreen;
+    public static LoadingScreenBar Instance;
 
     private int levelIndexToLoad = 0;
 
@@ -20,13 +20,20 @@ public class LoadingScreenBar : MonoBehaviour
 
     public void Awake()
     {
-        if(loadingScreen != null)
+        if(Instance != null)
         {
             Destroy(gameObject);
             return;
         }
 
-        loadingScreen = this;
+        Instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        Application.backgroundLoadingPriority = ThreadPriority.Low;
     }
 
     public void LoadLevel(int sceneIndex)
@@ -40,9 +47,21 @@ public class LoadingScreenBar : MonoBehaviour
         StartCoroutine(LoadAsynchronously(levelIndexToLoad));
     }
 
+    /// <summary>
+    /// Loads the next scene Asynchronously.
+    /// </summary>
+    /// <param name="sceneIndex">The scene index to load.</param>
+    /// <returns></returns>
     IEnumerator LoadAsynchronously(int sceneIndex)
     {
         bool load = false;
+
+        if (LoadingIconCo != null)
+        {
+            StopCoroutine(LoadingIconCo);
+        }
+
+        LoadingIconCo = StartCoroutine(FadeLoadingIcon(false));
 
         yield return new WaitForSeconds(0.1f);
 
@@ -52,7 +71,7 @@ public class LoadingScreenBar : MonoBehaviour
 
         while (!load)
         {
-            if (operation.progress >= 1.0f)
+            if (operation.progress >= 0.9f)
             {
                 load = true;
             }
@@ -60,17 +79,50 @@ public class LoadingScreenBar : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
 
+        operation.allowSceneActivation = true;
+
+        yield return new WaitUntil(() => operation.progress >= 1f);        
+
+        if (LoadingIconCo != null)
+        {
+            StopCoroutine(LoadingIconCo);
+        }
+
+        LoadingIconCo = StartCoroutine(FadeLoadingIcon(true));
+
         yield return new WaitForSeconds(levelLoadDelay);
 
-        operation.allowSceneActivation = true;
+        LevelFadeAnim.SetTrigger("FadeIn");        
     }
 
+    /// <summary>
+    /// Fades the Loading Icon in or out.
+    /// </summary>
+    /// <param name="fadeIn">Bool representation of fading in or out.</param>
+    /// <returns></returns>
     IEnumerator FadeLoadingIcon(bool fadeIn)
     {
+        LoadingIconImage.color = new Color(LoadingIconImage.color.r, LoadingIconImage.color.b, LoadingIconImage.color.g, (fadeIn ? 1f : 0f));
+
+        if (!fadeIn)
+        {
+            LoadingIconAnim.SetTrigger("IconLoop");
+        }
+
         for(float i = 0f; i < 1f; i += Time.deltaTime * (1f / LoadingIconFadeTime))
         {
-
+            LoadingIconImage.color = new Color(LoadingIconImage.color.r, LoadingIconImage.color.b, LoadingIconImage.color.g, 
+                Mathf.Lerp(LoadingIconImage.color.a, (fadeIn ? 0f : 1f), i));
             yield return null;
         }
+
+        LoadingIconImage.color = new Color(LoadingIconImage.color.r, LoadingIconImage.color.b, LoadingIconImage.color.g, (fadeIn ? 0f : 1f));
+
+        if (fadeIn)
+        {
+            LoadingIconAnim.SetTrigger("IconIdle");
+        }
+
+        LoadingIconCo = null;
     }
 }
