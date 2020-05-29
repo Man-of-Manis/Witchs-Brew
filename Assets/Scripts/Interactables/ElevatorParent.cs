40985faa4838a0225a7f21a31b7caa6bb89d02f0
Man-of-Manis <Man-of-Manis@users.noreply.github.com>
2020-05-29 13:49:04 -0700

    Small First Room changes

---
+++ Assets/Scripts/Interactables/ElevatorParent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorParent : MonoBehaviour
+{
+    public List<string> usableTags = new List<string>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach(string s  in usableTags)
+        {
+            if (other.CompareTag(s))
+            {
+                other.transform.parent = transform;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        foreach (string s in usableTags)
+        {
+            if (other.CompareTag(s))
+            {
+                other.transform.parent = null;
+            }
+        }
+    }
+}
